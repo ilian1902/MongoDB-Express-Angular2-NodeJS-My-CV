2ea8bdf0752602823e821f696863914bfbaddb1f
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-12 09:51:49 +0200

    Create node-express-mongodb server

---
+++ routes/skills.js
@@ -0,0 +1,83 @@
+let express = require('express');
+let router = express.Router();
+let mongojs = require('mongojs');
+const db = mongojs('mongodb://ilian82:limp0000@ds161028.mlab.com:61028/web_cv', ['skills']);
+
+// Get all skills
+router.get('/skills', function(req, res, next) {
+    db.skills.find(function(err, skills) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(skills);
+    });
+});
+
+// Get single skill 
+router.get('/skill/:id', function(req, res, next) {
+    db.skills.findOne({ _id: mongojs.ObjectId(req.params.id) }, function(err, skill) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(skill);
+    });
+});
+
+// Save skill
+
+router.post('/task', function(req, res, next) {
+    let skill = req.body;
+    if (!skill.title || !(skill.rate + "")) {
+        res.status(400);
+        res.json({
+            "error": "Bad data"
+        });
+    } else {
+        db.skills.save(skill, function(err, skill) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(skill);
+        });
+    }
+});
+
+// Delete skill
+router.delete('/skill/:id', function(req, res, next) {
+    db.skills.remove({ _id: mongojs.ObjectId(req.params.id) }, function(err, skill) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(skill);
+    });
+});
+
+// Update skill
+router.put('/skill/:id', function(req, res, next) {
+    let skill = req.body;
+    let updateSkill = {};
+
+    if (skill.title) {
+        updateSkill.title = skill.title;
+    }
+    if (skill.rate) {
+        updateSkill.rate = skill.rate;
+    }
+    if (!updateSkill) {
+        res.status(400);
+        res.json({
+            "error": "Bad data"
+        });
+    } else {
+        db.skills.remove({ _id: mongojs.ObjectId(req.params.id) }, function(err, skill) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(skill);
+        });
+    }
+
+
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let path = require('path');
+let bodyParser = require('body-parser');
+
+let index = require('./routes/index');
+let skills = require('./routes/skills');
+
+let app = express();
+
+let port = 3000;
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Body parser midal ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/api', skills);
+
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
