00011ffd225e17f332883880f06d9a138fc9b7c6
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-12 11:15:13 +0200

    Add skills.component.ts

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { SkillsComponent } from './app_page/skills/skills.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, SkillsComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
