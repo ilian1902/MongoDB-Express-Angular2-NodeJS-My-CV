9dbc9155ad62e522cdafded614abdfd708037c70
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-13 11:47:54 +0200

    Add image in home page

--- client/app/app_page/home/home.component.ts
+++ client/app/app_page/home/home.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'home',
   templateUrl: './home.component.html',
+  styleUrls: ['./../../app.component.css']
 })
 
 export class HomeComponent  {}
