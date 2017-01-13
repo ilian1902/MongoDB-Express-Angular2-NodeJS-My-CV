2411bd07bf5b09f81c73e7e895aaab0505eca3d8
ilian1902 <ilian.bratanov82@gmail.com>
2017-01-13 11:32:45 +0200

    Fix nav-bar

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -5,6 +5,7 @@ import { SkillService } from './app_core/services/skill.service';
   moduleId: module.id,
   selector: 'my-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   providers: [SkillService]
 })
 export class AppComponent  {
